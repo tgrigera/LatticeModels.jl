64c9b451e74e9ca4d533eee9b33c959493365104
Documenter.jl <documenter@juliadocs.github.io>
2023-02-25 19:27:29 +0000

    build based on 8862dec

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LatticeModels","category":"page"},{"location":"#LatticeModels","page":"Home","title":"LatticeModels","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LatticeModels.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LatticeModels]","category":"page"}]
+}
